b9fff45524ed57667259043dbbb5749c620d80c1
mayangming <mayangming@yuanxin-inc.com>
2019-08-15 13:38:53 +0800

    判断组件显示位置是否超出窗口范围

--- pages/test_grid/test_grid.js
+++ pages/test_grid/test_grid.js
@@ -5,14 +5,41 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    isHidden: 'hidden'
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const query = wx.createSelectorQuery()
+    query.select('#grid').boundingClientRect(rect => {
+      // rect.id      // 节点的ID
+      // rect.dataset // 节点的dataset
+      // rect.left    // 节点的左边界坐标
+      // rect.right   // 节点的右边界坐标
+      // rect.top     // 节点的上边界坐标
+      // rect.bottom  // 节点的下边界坐标
+      // rect.width   // 节点的宽度
+      // rect.height  // 节点的高度
+      var top = rect.top;
+      var windowH =wx.getSystemInfoSync().windowHeight;
+      var windowW = wx.getSystemInfoSync().windowWidth;
+      console.log('YM_屏幕可使用高度', windowH)
+      console.log('YM_屏幕可使用宽度', windowW)
+      console.log('YM_grid上边界坐标', top)
+      if (top >= windowH){
+        console.log('不在屏幕可视范围内');
+        this.setData({
+          isHidden: 'visible'
+        })
+      }else{
+        console.log('在屏幕可视范围内');
+        this.setData({
+          isHidden: 'hidden'
+        })
+      }
+    }).exec()
   },
 
   /**
