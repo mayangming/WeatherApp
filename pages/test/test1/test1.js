acce109110514952ff71c0618d43ed7bfac98cc5
YM_Home <banbianlian@7lk.com>
2019-09-15 09:39:56 +0800

    使用flex布局实现头部、尾部固定，中间滚动的布局

--- pages/test/test1/test1.js
+++ pages/test/test1/test1.js
@@ -13,7 +13,33 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    cardList: [
+      {
+        name: 'aa',
+        content: 'bb'
+      }, {
+        name: 'aa',
+        content: 'bb'
+      }, {
+        name: 'aa',
+        content: 'bb'
+      }, {
+        name: 'aa',
+        content: 'bb'
+      }, {
+        name: 'aa',
+        content: 'bb'
+      }, {
+        name: 'aa',
+        content: 'bb'
+      }, {
+        name: 'aa',
+        content: 'bb'
+      }, {
+        name: 'aa',
+        content: 'bb'
+      },
+    ]
   },
 
   /** 对象的拓展 */
